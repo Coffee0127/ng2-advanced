6a955b9d0d3105e33e1f62f9d88fd6ce4de7dc16
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-12 09:45:53 +0800

    實作第一層路由的 LayoutComponent 顯示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: LayoutComponent
   }
 ];
 
