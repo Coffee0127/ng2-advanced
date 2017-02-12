c62233b3dbac3127455708d8630891b113c8e555
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-12 09:57:42 +0800

    實作第一層路由的 Page2Component 顯示 ( 在瀏覽器網址列手動切換路由 )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent
-  }
+  { path: '', component: LayoutComponent },
+  { path: 'page2', component: Page2Component }
 ];
 
 @NgModule({
