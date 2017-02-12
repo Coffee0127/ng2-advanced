a819764e0ff6e2f30e270981a68a4081f884bf5a
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-12 15:42:42 +0800

    透過 Observable  取得上層路由參數變化及取消訂閱

--- src/app/flot/flot.component.ts
+++ src/app/flot/flot.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-flot',
   templateUrl: './flot.component.html',
   styleUrls: ['./flot.component.css']
 })
-export class FlotComponent implements OnInit {
+export class FlotComponent implements OnInit, OnDestroy {
 
   username: string;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
+  parentParams$: Subscription;
+
   ngOnInit() {
-    this.username = this.route.parent.snapshot.params['username'];
+    this.parentParams$ = this.route.parent.params.subscribe(params => {
+      this.username = params['username'];
+      console.log(this.username);
+    });
   }
 
+  ngOnDestroy() {
+    // 若未取消訂閱，因 route.parent 會一直存在，故會造成重複訂閱
+    this.parentParams$.unsubscribe();
+  }
 }
