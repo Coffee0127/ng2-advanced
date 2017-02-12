eee8bdd81991f59783a0d6ae5fed7503738cd30c
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-12 14:50:34 +0800

    在相同路由之間切換不同參數 ( 改用 this.route.params 訂閱參數變更 )

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -13,8 +13,16 @@ export class CardsComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // 透過 Observable 監看 params 變化
+    this.route.params.subscribe(params => {
+      this.type = params['type'];
+    });
     // 路由執行當下的資料 (僅初始值)
-    this.type = this.route.snapshot.params['type'] || this.route.snapshot.data['type'];
+    // 避免因使用 data 覆蓋 Observable 帶回來的 type
+    this.type = this.type || this.route.snapshot.data['type'];
   }
 
+  goCards(type) {
+    this.router.navigateByUrl('/cards/' + type);
+  }
 }
