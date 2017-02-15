752cc0187b3513a4eadb3500d7c4413f9d5af74e
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-15 23:01:42 +0800

    export initDashboard 並於 dashboard.component.ts 執行

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { initDashboard } from '../../assets/js/app.js';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,6 +13,7 @@ export class DashboardComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    initDashboard();
   }
 
   goCards(type) {
