c9ac4ae694433a81d5f99b68aab210cf816941cd
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-19 12:02:49 +0800

    透過 FormArray 搭配 *ngFor 產生動態表單結構

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { FormArray, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -13,12 +13,21 @@ export class ReactiveFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       'name': ['Xuan', Validators.required],
-      'm1': ['', [Validators.required, Validators.minLength(2), Validators.maxLength(4)]]
+      'm1': ['', [Validators.required, Validators.minLength(2), Validators.maxLength(4)]],
+      'group1': this.fb.array([
+        this.fb.control('Xuan 1', Validators.required),
+        this.fb.control('Xuan 2', Validators.required),
+        this.fb.control('Xuan 3', Validators.required),
+        this.fb.control('Xuan 4', Validators.required),
+      ])
     });
   }
 
   ngOnInit() {
     this.form.addControl('email', this.fb.control('default@example.com', Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)));
+
+    let group1: FormArray = <FormArray> this.form.controls['group1'];
+    group1.insert(group1.length, this.fb.control('Xuan 5'));
   }
 
   getFieldInvalid(fieldName) {
